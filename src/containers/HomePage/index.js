65e55ff7ea76c7d27a4a362c3daa9e46c93a84cf
adamska27 <adams.roy27@gmail.com>
2018-03-15 14:32:51 +0100

    (HomePage/index): create the index file, wich contains the logic of the home page, get some state from the store'state and action to dispatch and finally pass it as props to the PopularGames component with connect (from react-redux)

---
+++ src/containers/HomePage/index.js
@@ -0,0 +1,20 @@
+import { connect } from 'react-redux';
+
+import { fetchPopularGames } from './actions';
+import PopularGames from '../../components/PopularGames';
+
+const mapStateToProps = state => ({ 
+    popularGames: state.popularGames.popularGames,
+    isFetching: state.popularGames.isFetching,
+});
+
+const mapDispatchToProps = dispatch => ({ 
+    fetchPopularGames: () => dispatch(fetchPopularGames())
+});
+
+const HomePage = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(PopularGames);
+
+export default HomePage;
