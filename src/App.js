82d00a4ae7e1b65996164708c4e202faff9b0982
adamska27 <adams.roy27@gmail.com>
2018-03-15 14:48:51 +0100

    (App): import the HomePage container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import HomePage from './containers/HomePage';
 
 class App extends Component {
   render() {
     return (
       <div>
         <p>Hello world</p>
+        <HomePage />
       </div>
     );
   }
