4af2816b16407d2d5297eae017a107246575b23e
adamska27 <adams.roy27@gmail.com>
2018-03-16 16:33:03 +0100

    style(PopularGames): start to style the component: test css grid

--- src/components/PopularGames.jsx
+++ src/components/PopularGames.jsx
@@ -1,4 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import getBetterCover from '../utils/getBetterCover';
+
+const Container = styled.ul`
+    display: grid;
+    grid-auto-columns: min-content;
+    /* grid-auto-rows: 1fr; */
+    grid-template-areas: "game game game game";
+    grid-template-rows: 1fr;    
+    list-style-type: none;
+`
+
+const Cover = styled.img`
+    grid-area: cover;
+`
+
+const Game = styled.div`
+    display: grid;
+    grid-area: game;
+    grid-template-areas: 
+    "cover"
+    "title"
+    ;
+`
+
+const Title = styled.p`
+    grid-area: title;
+    font-size: 11px;
+    font-weight: normal;
+    text-align: center;
+`
 
 export default class PopularGames extends React.Component {
 
@@ -11,9 +43,11 @@ export default class PopularGames extends React.Component {
     }
 
     PopularGamesItem = (game) => {
-        return (<div>
-                    <p>{game.name}</p>
-                </div>)
+        const cover = getBetterCover(game.cover.url);
+        return (<Game>
+                    <Cover src={cover} />
+                    <Title>{game.name}</Title>
+                </Game>)
     }
 
     render() {
@@ -24,15 +58,13 @@ export default class PopularGames extends React.Component {
         };
 
         return(
-            <div>
-                <ul>
-                    {popularGames.map( game => {
-                        return <li key={game.id}>
-                            {this.PopularGamesItem(game)}
-                        </li>
-                    })}
-                </ul>
-            </div>
+            <Container>
+                {popularGames.map( game => {
+                    return <li key={game.id}>
+                        {this.PopularGamesItem(game)}
+                    </li>
+                })}
+            </Container>
         )
     }
 };
