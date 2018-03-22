f5f0b65f7bc81d1e3598694b386f1196208e33af
adamska27 <adams.roy27@gmail.com>
2018-03-22 14:36:27 +0100

    (App): add router and basic routes for test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import store from './store';
 
+import Header from './components/Header';
 import HomePage from './containers/HomePage';
 
 class App extends Component {
-render() {
+  render() {
     return (
       <Provider store={store}>
-        <div>
-          <p>Hello world</p>
-          <HomePage />
-        </div>
+        <Router>
+          <React.Fragment>
+            <Header />
+            <Route exact path="/" component={HomePage} />
+            <Route
+              path="/game/:gameId"
+              render={({ match }) => (
+                <div>
+                  <h1>{match.params.gameId}</h1>
+                </div>
+              )}
+            />
+          </React.Fragment>
+        </Router>
       </Provider>
     );
   }
