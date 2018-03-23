b856b0d13eb2b5919579b9533ce113ad4e61246b
adamska27 <adams.roy27@gmail.com>
2018-03-23 15:35:38 +0100

    (App): Import and Use Redirect: If a bad url is enter, redirect to the HomePage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
 import store from './store';
@@ -26,6 +26,7 @@ class App extends Component {
                   </div>
                 )}
               />
+              <Redirect from="*" to="/" component={HomePage} />
             </React.Fragment>
           </ThemeProvider>
         </Router>
