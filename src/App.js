2ca33b3b1754dfeb5278f395a226928f117350c0
adamska27 <adams.roy27@gmail.com>
2018-03-23 12:01:53 +0100

    style(App): create theme.js in styles folder and import it, import ThemeProvider from styled-components, pass the theme imported as props theme to the provider and wrapp all the components in, now we have access at the props theme in all the components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 import store from './store';
+import { theme } from './styles/theme';
 
 import Header from './components/Header/index';
 import HomePage from './containers/HomePage';
@@ -12,18 +14,20 @@ class App extends Component {
     return (
       <Provider store={store}>
         <Router>
-          <React.Fragment>
-            <Header />
-            <Route exact path="/" component={HomePage} />
-            <Route
-              path="/game/:gameId"
-              render={({ match }) => (
-                <div>
-                  <h1>{match.params.gameId}</h1>
-                </div>
-              )}
-            />
-          </React.Fragment>
+          <ThemeProvider theme={theme}>
+            <React.Fragment>
+              <Header />
+              <Route exact path="/" component={HomePage} />
+              <Route
+                path="/game/:gameId"
+                render={({ match }) => (
+                  <div>
+                    <h1>{match.params.gameId}</h1>
+                  </div>
+                )}
+              />
+            </React.Fragment>
+          </ThemeProvider>
         </Router>
       </Provider>
     );
