afa6d12b25fc4b59b23015780e2376e9faecb7cb
adamska27 <adams.roy27@gmail.com>
2018-03-29 21:33:33 +0200

    fix(NotFound): remove props

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 
 export default class NotFound extends React.PureComponent {
   render() {
-    const { pathname } = this.props.location;
     return (
       <div>
-        <h2>La page {pathname} n'existe pas ! :(</h2>
+        <h2>La page n'existe pas ! :(</h2>
       </div>
     );
   }
