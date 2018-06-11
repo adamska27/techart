a7795b78098efb0b9927bf7a790146a69144577a
adamska27 <adams.roy27@gmail.com>
2018-06-11 15:06:30 +0200

    (reducers) add the new reducers created

--- src/reducers.js
+++ src/reducers.js
@@ -4,13 +4,17 @@ import { game } from './containers/GameContainer/reducers';
 import { navMobile } from './containers/Header/reducers';
 import { popularGames } from './containers/PopularGames/reducers';
 import { recentGames } from './containers/RecentGames/reducers';
+import { userOfTheWeek } from './containers/UserOfTheWeek/reducers';
+import { latestUsers } from './containers/LatestUsers/reducers';
 
 const reducers = combineReducers({
   account,
   game,
+  latestUsers,
   navMobile,
   popularGames,
-  recentGames
+  recentGames,
+  userOfTheWeek
 });
 
 export default reducers;
