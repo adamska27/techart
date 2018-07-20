d57eced1d1cffa7fe478b2438af484880c38410a
adamska27 <adams.roy27@gmail.com>
2018-07-20 22:41:40 +0200

    feat(App): add the new route + add the new reducers

--- src/App.js
+++ src/App.js
@@ -9,10 +9,12 @@ import { theme } from './styles/theme';
 import Footer from './components/Footer';
 import HeaderContainer from './containers/Header';
 
+import Collection from './pages/Collection';
 import Game from './pages/Game';
 import Home from './pages/Home';
 import LogIn from './pages/LogIn';
 import PagesNotFound from './pages/PagesNotFound';
+import Profile from './pages/Profile';
 import RatingsForm from './pages/RatingsForm';
 import SignUp from './pages/SignUp';
 
@@ -30,6 +32,8 @@ class App extends Component {
                 <Route path="/:gameId/rating" component={RatingsForm} />
                 <Route path="/signup" component={SignUp} />
                 <Route path="/login" component={LogIn} />
+                <Route path="/user/:id" component={Profile} />
+                <Route path="/collection" component={Collection} />
                 <Route component={PagesNotFound} />
               </Switch>
               <Footer />
--- src/reducers.js
+++ src/reducers.js
@@ -1,9 +1,12 @@
-import { account } from './containers/AccountContainer/reducers';
 import { combineReducers } from 'redux';
+
+import { account } from './containers/AccountContainer/reducers';
+import { collection } from './containers/Collection/reducers';
 import { game } from './containers/GameContainer/reducers';
 import { latestUsers } from './containers/LatestUsers/reducers';
 import { navMobile } from './containers/Header/reducers';
 import { popularGames } from './containers/PopularGames/reducers';
+import { profile } from './containers/Profile/reducers';
 import { ratings } from './containers/Ratings/reducers';
 import { recentGames } from './containers/RecentGames/reducers';
 import { submitRatings } from './containers/RatingsFormContainer/reducers';
@@ -11,10 +14,12 @@ import { userOfTheWeek } from './containers/UserOfTheWeek/reducers';
 
 const reducers = combineReducers({
   account,
+  collection,
   game,
   latestUsers,
   navMobile,
   popularGames,
+  profile,
   ratings,
   recentGames,
   submitRatings,
