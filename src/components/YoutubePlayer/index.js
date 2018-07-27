dde0c549b86f3dffe49d96fc9bde4cf6214c88de
adamska27 <adams.roy27@gmail.com>
2018-07-27 13:52:08 +0200

    fix(YoutubePlayer): remove useless init function

--- src/components/YoutubePlayer/index.js
+++ src/components/YoutubePlayer/index.js
@@ -8,28 +8,7 @@ export default class YoutubePlayer extends React.PureComponent {
   };
 
   componentDidMount() {
-    this.init();
     reframe('iframe');
-  }
-
-  init() {
-    // check youtube-api script
-    var scripts = document.getElementsByTagName('script');
-    const scriptsArr = Array.from(scripts);
-    let checkScript = true;
-    scriptsArr.map(script => {
-      if (script.src === 'https://www.youtube.com/iframe_api') {
-        return (checkScript = false);
-      }
-      return checkScript;
-    });
-
-    if (checkScript) {
-      var tag = document.createElement('script');
-      tag.src = 'https://www.youtube.com/iframe_api';
-      var firstScriptTag = document.getElementsByTagName('script')[0];
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-    }
   }
 
   render() {
@@ -41,7 +20,7 @@ export default class YoutubePlayer extends React.PureComponent {
           frameBorder="0"
           height="360"
           id="player"
-          src={`http://www.youtube.com/embed/${videoId}?enablejsapi=1&origin=http://localhost:3000`}
+          src={`http://www.youtube.com/embed/${videoId}?autoplay=1&origin=http://localhost:3000`}
           title="youtube-iframe"
           type="text/html"
           width="640"
